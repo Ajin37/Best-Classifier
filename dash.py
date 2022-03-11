c2be3d300f11fb6bd2c53103d8613d6ef63fa061
Ajin37 <99724818+Ajin37@users.noreply.github.com>
2022-03-11 23:11:48 +0530

    Create dash.py

---
+++ dash.py
@@ -0,0 +1,25 @@
+site_dropdown,payload_slider):
+    if site_dropdown == 'All Sites':
+        low, high = payload_slider
+        df  = spacex_df
+        mask = (df['Payload Mass (kg)'] > low) & (df['Payload Mass (kg)'] < high)
+        fig = px.scatter(
+            df[mask], x="Payload Mass (kg)", y="class",
+            color="Booster Version",
+            size='Payload Mass (kg)',
+            hover_data=['Payload Mass (kg)'])
+    else:
+        low, high = payload_slider
+        df  = spacex_df.loc[spacex_df['Launch Site'] == site_dropdown]
+        mask = (df['Payload Mass (kg)'] > low) & (df['Payload Mass (kg)'] < high)
+        fig = px.scatter(
+            df[mask], x="Payload Mass (kg)", y="class",
+            color="Booster Version",
+            size='Payload Mass (kg)',
+            hover_data=['Payload Mass (kg)'])
+    return fig
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=False)
